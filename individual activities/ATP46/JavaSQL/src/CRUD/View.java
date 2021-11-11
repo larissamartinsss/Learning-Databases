b3b667c341a42fc367da28dc5d26f7c8d6bcdba7
Larissa Martins <larissamartins.job@gmail.com>
2021-11-11 16:26:24 -0400

    Banco conectado

--- individual activities/ATP46/JavaSQL/src/CRUD/View.java
+++ individual activities/ATP46/JavaSQL/src/CRUD/View.java
@@ -8,10 +8,19 @@ public class View {
     public static void main(String[] args) {
         // 1º step:
         try {
-            Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "123456");
+            String driverType = "jdbc";  String driverName = "postgresql";
+            String host = "localhost";   String database = "postgres";
+            String user = "postgres";    String pwd = "123456";
+            int port = 5432;
+
+            String connectionString = String.format("%s:%s://%s:%d/%s", driverType,driverName, host, port, database);
+            Connection connection = DriverManager.getConnection(connectionString, user, pwd);
+            connection.close();
             System.out.println("Banco de dados conectado! ");
         } catch (SQLException e) {
             System.out.println("Não é possível conectar ao Banco de Dados. ");
         }
     }
 }
+
+
