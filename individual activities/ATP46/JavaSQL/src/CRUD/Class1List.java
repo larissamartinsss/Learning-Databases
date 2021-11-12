0a329aaebd7c566804479b5bdd8d0906c2b26ddc
Larissa Martins <larissamartins.job@gmail.com>
2021-11-12 12:09:31 -0400

    ATP46

--- individual activities/ATP46/JavaSQL/src/CRUD/Class1List.java
+++ individual activities/ATP46/JavaSQL/src/CRUD/Class1List.java
@@ -1,8 +1,40 @@
 package CRUD;
 
 
+import java.sql.*;
 
 public class Class1List {
+    public static void main(String[] args) {
+        // 1º step:
+        try {
+            String driverType = "jdbc";  String driverName = "postgresql";
+            String host = "localhost";   String database = "postgres";
+            String user = "postgres";    String pwd = "123456";
+            int port = 5432;
+            String connectionString = String.format("%s:%s://%s:%d/%s", driverType,driverName, host, port, database);
+            // Conectando:
+            Connection connection = DriverManager.getConnection(connectionString, user, pwd);
+            // Acessando DB - Query SQL:
+            Statement statement = connection.createStatement();
+            statement.execute("SELECT * FROM PRODUTO");
+            //Buscar Retorno do statement:
+            ResultSet result = statement.getResultSet();
+            while (result.next()) {
+                int id = result.getInt("id");
+                String nome = result.getString("nome");
+                String descricao = result.getString("descricao");
+                float preco = result.getFloat("preco");
+                int categoria_id = result.getInt("categoria_id");
 
+               String.format("\nId: %d \n Nome: %s \n Descrição: %s \n Valor: R$%.2f \n Categoria ID: %d", id, nome, descricao, preco, categoria_id);
 
+            }
+            connection.close();
+            System.out.println("Banco de dados conectado! ");
+        } catch (SQLException e) {
+            System.out.println("Não é possível conectar ao Banco de Dados. ");
+        }
+    }
 }
+
+
--- individual activities/ATP46/JavaSQL/src/CRUD/Class2Add.java
+++ individual activities/ATP46/JavaSQL/src/CRUD/Class2Add.java
@@ -1,5 +1,38 @@
 package CRUD;
 
-public class Class2Add {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
+public class Class2Add {
+    public static void main(String[] args) {
+
+        try {
+            String driverType = "jdbc";  String driverName = "postgresql";
+            String host = "localhost";   String database = "postgres";
+            String user = "postgres";    String pwd = "123456";
+            int port = 5432;
+            String connectionString = String.format("%s:%s://%s:%d/%s", driverType,driverName, host, port, database);
+            // Conectando:
+            Connection connection = DriverManager.getConnection(connectionString, user, pwd);
+
+
+            Statement statement = connection.createStatement();
+            String sql = "INSERT INTO produto(nome,descricao,preco,categoria_id)VALUES('Mi band','5th','250',2)";
+
+            statement.execute(sql, statement.RETURN_GENERATED_KEYS);
+            ResultSet ids = statement.getGeneratedKeys();
+
+            while (ids.next()) {
+                int id = ids.getInt(1);
+                String.format("\nProduto com id nº: %s inserido com sucesso! ",id);
+            }
+
+            connection.close();
+        } catch (Exception e) {
+            System.out.println("Não é possível conectar ao Banco de Dados. ");
+        }
+    }
 }
+
