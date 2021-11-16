f740d23d9f0905d3c5bbb5c2d76b429cf528dc81
Larissa Martins <larissamartins.job@gmail.com>
2021-11-16 19:22:13 -0400

    data source

--- individual activities/ATP50/src/utils/ConnectionFactory.java
+++ individual activities/ATP50/src/utils/ConnectionFactory.java
@@ -2,6 +2,7 @@ package utils;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -10,6 +11,7 @@ public class ConnectionFactory {
     String driverType = "jdbc";  String driverName = "postgresql";
     String host = "localhost";   String database = "postgres";
     String user = "postgres";    String pwd = "123456";
+    private DataSource dataSource;
     int port = 5432;
     String connectionString = String.format("%s:%s://%s:%d/%s", driverType,driverName, host, port, database);
 
@@ -20,9 +22,10 @@ public class ConnectionFactory {
         poll.setUser(this.user);
         poll.setPassword(this.pwd);
         poll.setMaxPoolSize(50);
+        this.dataSource = poll;
     }
 
     public Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(connectionString, user, pwd);
+        return this.dataSource.getConnection();
     }
 }
